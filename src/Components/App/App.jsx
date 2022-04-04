edb514de5e8eb33ae61b4e69aec451a3e479a2da
Antonio Riccelli <anton.riccelli@gmail.com>
2022-04-04 17:40:20 +0100

    initial version of the score component. Functional

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,18 +2,20 @@
 import styles from './App.module.css';
 import Header from "../Header";
 import Board from "../Board";
-import Modal from "../Modal/index.jsx"
+import Score from "../Score/index.jsx"
 import {useState} from "react";
 
 
 function App() {
-
+const [score, setScore] = useState({"player": 0, "computer": 0});
 
  
   return (
     <div className={styles.App}>
+    <Score score={score} />
      <Header />
-     <Board />
+     
+     <Board setScore={setScore} score={score}/>
     </div>
   );
 }
